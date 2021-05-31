bf3d564c60103002546552ee62406ae0ccb48830
Isaac Ellmen <isaacellmen@gmail.com>
2021-05-31 15:25:43 -0400

    Filled in setup.py info

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setup(
     name='alcov',
-    version='0.1',
+    version='0.1.1',
     description='Identify frequencies of concerning mutations from aligned reads',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Isaac Ellmen',
     author_email='isaac.ellmen@uwaterloo.ca',
     packages=['alcov'],
+    url='https://github.com/Ellmen/alcov',
     install_requires=[
         'fire',
         'numpy',
