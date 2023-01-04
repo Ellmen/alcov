df2818dd813d3c26b58968b0065379d9f93183be
JennKnapp <jknapp@uwaterloo.ca>
2023-01-04 12:42:40 -0500

    Fixed find_mutants csv and img saving

--- alcov/__init__.py
+++ alcov/__init__.py
@@ -18,7 +18,7 @@ class CovBreakdown(object):
         nt(mut)
 
     def find_mutants(self, samples_path, mutations_path=None, min_depth=40, save_img=False, csv=False):
-        find_mutants(samples_path, mutations_path, min_depth, save_img, csv)
+        find_mutants(samples_path, mutations_path=mutations_path, min_depth=min_depth, save_img=save_img, csv=csv)
 
     def find_lineages(self, samples_path, lineages_path=None, ts=False, csv=False, min_depth=40, show_stacked=False, unique=False, save_img=False, l2=False):
         find_lineages(samples_path, lineages_path=lineages_path, ts=ts, csv=csv, min_depth=min_depth, show_stacked=show_stacked, unique=unique, save_img=save_img, l2=l2)
