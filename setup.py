6df6481e437d8e751f282fa9f5da384e9a840ed0
JennKnapp <jknapp@uwaterloo.ca>
2023-06-27 16:39:35 -0400

    Only lineages predicted to be over 1% will be included in the heatmap. All predicitons will still be present in the csv.

--- setup.py
+++ setup.py
@@ -5,12 +5,14 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='alcov',
-    version='0.1.1',
+    version='1.1.1',
     description='Identify frequencies of concerning mutations from aligned reads',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Isaac Ellmen',
     author_email='isaac.ellmen@uwaterloo.ca',
+    maintainer='Jenn Knapp',
+    maintainer_email='jenn.knapp@uwaterloo.ca',
     packages=['alcov'],
     url='https://github.com/Ellmen/alcov',
     install_requires=[
