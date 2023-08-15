c267343b755303be5f39aef7c0bac6c7d54c6f95
JennKnapp <jknapp@uwaterloo.ca>
2023-08-15 15:11:25 -0400

    Removed unnecessary file

--- alcov/make_constellations.py
+++ alcov/make_constellations.py
@@ -42,20 +42,29 @@ for lineage in lineages:
 
 # Construct URL
     url1 = base_url + "/sample/aggregated?nextcladePangoLineage=" + lineage
+    url3 = base_url + "/sample/aggregated?pangoLineage=" + lineage
 
     try:
         # Send API request
-        response = requests.get(url1)
+        response1 = requests.get(url1)
 
         # Check if the request was successful (status code 200)
-        if response.status_code == 200:
+        if response1.status_code == 200:
             # Parse the response as JSON
-            data1 = response.json()
+            data1 = response1.json()
+        
+        # Send API request
+        response3 = requests.get(url3)
+       
+        if response1.status_code == 200:
+            # Parse the response as JSON
+            data3 = response3.json()
 
             # Extract the required data from the JSON response
-            count = data1["data"][0]["count"]
+            count1 = data1["data"][0]["count"]
+            count3 = data3["data"][0]["count"]
 
-            if count > 100:
+            if count1 > 100:
 
                 # Construct URL
                 url2 = base_url + "/sample/nuc-mutations?nextcladePangoLineage=" + lineage
@@ -63,6 +72,45 @@ for lineage in lineages:
                 try:
                     # Send API request
                     response = requests.get(url2)
+
+                    # Check if the request was successful (status code 200)
+                    if response.status_code == 200:
+                        # Parse the response as JSON
+                        data = response.json()
+
+                        # Extract the required data from the JSON response
+                        nucleotide_mutations = [
+                            mutation["mutation"] for mutation in data["data"]
+                            if mutation["proportion"] > 0.90 and not mutation["mutation"].endswith("-")
+                        ]
+
+                        # Create the final JSON structure
+                        json_data = {
+                            "label": lineage + "-like",
+                            "description": f"{lineage} lineage defining mutations",
+                            "sources": [],
+                            "tags": [lineage],
+                            "sites": nucleotide_mutations,
+                            "note": "Unique mutations for sublineage"
+                        }
+
+                        # Write data to JSON file
+                        with open(output_file, "w") as file:
+                            json.dump(json_data, file)
+                    else:
+                        print("Error occurred while fetching data for lineage:", lineage)
+         
+                except requests.RequestException as e:
+                        print("An error occurred during the API request:", str(e))
+
+            elif count3 > 100:
+
+                # Construct URL
+                url4 = base_url + "/sample/nuc-mutations?pangoLineage=" + lineage
+
+                try:
+                    # Send API request
+                    response = requests.get(url4)
 
                     # Check if the request was successful (status code 200)
                     if response.status_code == 200:
