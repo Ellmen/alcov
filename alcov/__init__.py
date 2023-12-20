38a2c504fb971c1eb0b2e8e61e7a7515e793f0bc
Jenn Knapp <jenn.knapp@uwaterloo.ca>
2023-12-20 10:49:29 -0500

    Fixed issue with git-lfs

--- alcov/__init__.py
+++ alcov/__init__.py
@@ -1,3 +1,31 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0d9ce4f70f9e8cb3f93e9d2e92957a8e90e8ca46886f459aca97e02684e1e965
-size 1121
+from .amplicon_coverage import amplicon_coverage, gc_depth
+from .analyze import find_mutants
+from .cmds import aa, nt
+from .lineages import find_lineages
+# from .consensus import consensus_from_bam
+
+
+class CovBreakdown(object):
+    """Identify frequencies of concerning mutations from aligned reads"""
+
+    def __str__(self):
+        return "Identify frequencies of concerning mutations from aligned reads"
+
+    def aa(self, mut):
+        aa(mut)
+
+    def nt(self, mut):
+        nt(mut)
+
+    def find_mutants(self, samples_path, mutations_path=None, min_depth=40, save_img=False, csv=False):
+        find_mutants(samples_path, mutations_path=mutations_path, min_depth=min_depth, save_img=save_img, csv=csv)
+
+    def find_lineages(self, samples_path, lineages_path=None, ts=False, csv=False, min_depth=40, show_stacked=False, unique=F$
+        find_lineages(samples_path, lineages_path=lineages_path, ts=ts, csv=csv, min_depth=min_depth, show_stacked=show_stack$
+
+    def amplicon_coverage(self, samples_path):
+        amplicon_coverage(samples_path)
+
+    def gc_depth(self, samples_path):
+        gc_depth(samples_path)
+
--- setup.py
+++ setup.py
@@ -1,3 +1,33 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3b1240f169f31060fe096771f35ddd4f9dd04f40882e937be81adde72c0cfc5e
-size 863
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='alcov',
+    version='1.1.23',
+    description='Identify frequencies of concerning mutations from aligned reads',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Isaac Ellmen',
+    author_email='isaac.ellmen@uwaterloo.ca',
+    maintainer='Jenn Knapp',
+    maintainer_email='jenn.knapp@uwaterloo.ca',
+    packages=['alcov'],
+    exclude_package_data={
+	'alcov': ['data'],
+	},
+    url='https://github.com/Ellmen/alcov',
+    install_requires=[
+        'fire',
+        'numpy',
+        'pandas',
+        'scikit-learn>=0.24',
+        'matplotlib',
+        'seaborn',
+        'pysam',
+    ],
+    entry_points={
+        'console_scripts': ['alcov=alcov.command_line:main'],
+    }
+)
