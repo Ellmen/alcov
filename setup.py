70be8b739245e8baa7d9b0065998b8ba140f10e9
JennKnapp <jknapp@uwaterloo.ca>
2024-09-09 16:18:53 -0400

    Constellation update 20240909

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='alcov',
-    version='1.1.32',
+    version='1.1.33',
     description='Identify frequencies of concerning mutations from aligned reads',
     long_description=long_description,
     long_description_content_type='text/markdown',
